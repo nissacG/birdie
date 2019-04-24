eda290902a44b5a9c025569d2d5d48da7a968430
Gareth Cassin <nissacjg@gmail.com>
2019-04-24 17:41:40 +0100

    MySQL and connection set up

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require('express')
+const mysql = require('mysql')
+const config = require('./config')
+
+// MySQL Connection
+const db = mysql.createConnection({
+  host     : config.host,
+  port     : config.port,
+  user     : config.user,
+  password : config.password
+})
+
+// Db connection test
+db.connect((err) => {
+  if (err) {
+    console.log('err:', err)
+  }
+  console.log('Db connected...')
+})
+
 const app = express()
 
 const port = 5000
 
+// Server Heath Check
 app.get('/hc', (req, res) => {
   const running = `I'm alive!`
-  res.json(running)
+  res.send(running)
 })
 
 app.listen(port, () => console.log(`Serving on Port ${port}`))
